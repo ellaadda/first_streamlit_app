375565da00869f9d836f8d8f765a8b9a88d23bdf
ellaadda <123548225+ellaadda@users.noreply.github.com>
2023-03-21 09:11:53 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Breakfast Menu')
-streamlit('Omega 3 & Blueberry Oatmeal')
-streamlit('Kale, Spinach & Rocket Smoothie')
-stramlit('Hard-Boiled Free-Range Egg')
+streamlit.lower('Omega 3 & Blueberry Oatmeal')
+streamlit.lower('Kale, Spinach & Rocket Smoothie')
+stramlit.lower('Hard-Boiled Free-Range Egg')
