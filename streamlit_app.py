8a951d7ddba576b9ece2f30e2ff52d24e2755405
ellaadda <123548225+ellaadda@users.noreply.github.com>
2023-03-21 17:31:26 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
